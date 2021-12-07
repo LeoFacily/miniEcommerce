ebb5496b2c8418914178b932d31df074ce0126c4
unknown <leo.monte@faci.ly>
2021-12-07 08:17:09 -0300

    Regra User - Customer

--- app/services/customer_service.py
+++ app/services/customer_service.py
@@ -1,13 +1,19 @@
 from fastapi import Depends
+from sqlalchemy.sql.functions import user
 from app.api.customer.schemas import CustomerSchema
+from app.models.models import User
 from app.repositories.customer_repository import CustomerRepository
+from app.repositories.user_repository import UserRepository
 
 class CustomerService:
-    def __init__(self, customer_repository: CustomerRepository = Depends()):
+    def __init__(self, customer_repository: CustomerRepository = Depends(), user_repository: UserRepository = Depends()):
         self.customer_repository = customer_repository
+        self.user_repository = user_repository
 
     def create_customer(self, customer: CustomerSchema):
-        CustomerRepository.create(**customer.dic())
+        if not self.user_repository.query(User).filter(User.email == customer.email):
+            UserRepository.create(**user.dict())
+            CustomerRepository.create(**customer.dic())
                 
     def update_customer(self, customer: CustomerSchema):
         CustomerRepository.update()
