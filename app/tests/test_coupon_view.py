4aeff30785450e469da616acd44a12f90c73c430
unknown <leo.monte@faci.ly>
2021-12-15 17:29:14 -0300

    ...

--- app/tests/test_coupon_view.py
+++ app/tests/test_coupon_view.py
@@ -9,7 +9,7 @@ def test_coupon_create(client: TestClient, coupon_factory):
 
     response = client.post('/coupon/',
                             json={
-                                'code':'teste',
+                                'code':'4A5B6C',
                                 'expire_at':'3/14/2016',
                                 'limit':1,
                                 'mode':'VALUE',
@@ -17,4 +17,26 @@ def test_coupon_create(client: TestClient, coupon_factory):
                             })
 
     assert response.status_code == 201
-    assert response.json()['code'] == 'teste'
+    assert response.json()['code'] == '4A5B6C'
+
+def test_coupon_update(client: TestClient, coupon_factory):
+    coupon = coupon_factory()
+    
+    response = client.post('/coupon/',
+                            json={
+                                'code':'4A5B6C',
+                                'expire_at':'3/14/2016',
+                                'limit':1,
+                                'mode':'VALUE',
+                                'value':10.0
+                            })
+    
+    assert response.status_code == 201
+    assert response.json()['code'] == '4A5B6C'
+
+    response = client.put(
+        '/coupon/1',
+        json={'description': '4A5B6C Alterado!!'})
+
+    assert response.status_code == 200
+    assert response.json()['name'] == '4A5B6C Alterado!!'
--- app/tests/test_product_view.py
+++ app/tests/test_product_view.py
@@ -20,3 +20,28 @@ def test_product_create(client: TestClient, category_factory, supplier_factory, 
     assert response.json()['description'] == 'descricao'
     assert response.json()['category_id'] == category.id
     assert response.json()['supplier_id'] == supplier.id
+
+
+def test_product_update(client: TestClient, category_factory, supplier_factory):
+    category = category_factory()
+    supplier = supplier_factory()
+    
+    response = client.post('/products/',  
+                            json={
+                               'description': 'descricao',
+                               'price': 100,
+                               'image': 'image.dev',
+                               'technical_details': 'bla bla',
+                               'visible': True,
+                               'category_id': category.id,
+                               'supplier_id': supplier.id
+                           })
+    
+    assert response.status_code == 201
+
+    response = client.put(
+        '/products/1',
+        json={'description': 'descricao Alterada!!'})
+
+    assert response.status_code == 200
+    assert response.json()['name'] == 'descricao Alterada!!!!'
